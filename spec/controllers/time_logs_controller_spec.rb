52c0f372efcd9a43ef362d55551b1d29322a6885
John Apostol <john.apostol@idrivemarketing.com>
2014-07-19 13:54:45 -0400

    Write time log controller tests

--- spec/controllers/time_logs_controller_spec.rb
+++ spec/controllers/time_logs_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe TimeLogsController, :type => :controller do
+RSpec.describe TimeLogsController, type: :controller do
+
+  describe 'as an admin' do
+    before (:each) do
+      @user = FactoryGirl.create :user_with_time_log
+      @time_log = @user.time_logs.first
+      sign_in @user
+    end
+
+    describe "POST 'create'" do
+      it "returns http success" do
+        xhr :post, :create, {time_log: {user: @user}}
+        expect(response).to be_success
+      end
+    end
+
+    describe "POST 'update'" do
+      it "returns http success" do
+        xhr :post, :update, {id: @user.id, time_log: {id: @time_log.id}}
+        expect(response).to be_success
+      end
+    end
+  end
 
 end
